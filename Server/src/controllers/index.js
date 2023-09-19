6cf0995ffdb3faf17b6837613e1270bd3744f969
LiliSLG <silvialilianagarcia@hotmail.com>
2023-09-19 18:02:53 -0300

    Separamos repositorios de front y back

--- Server/src/controllers/index.js
+++
@@ -1,5 +0,0 @@
-const { getCharById } = require("./getCharById");
-const { login } = require("./login");
-const { postFav, deleteFav } = require("./handleFavorites");
-
-module.exports = { getCharById, login, postFav, deleteFav };
